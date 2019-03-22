99c8019c56aa51a21573f26ada84adff9bcc5a59
Surbhiairan <surbhiairan1@gmail.com>
2019-03-22 14:59:48 +0530

    my collection api

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -7,8 +7,10 @@ export const URI = {
     PROFILE: 'userprofile',
     LOGOUT: 'core/logout/',
 
-    USER_COLLECTION: 'collection/?user_id={0}',
+    USER_COLLECTION: 'user/{0}/collection',
     COLLECTION: 'collection',
+
+
 
     GET_FEEDS: 'https://5c722f0dba65bb0014ebefb0.mockapi.io/getFeeds',
     POST: 'post',
@@ -17,8 +19,8 @@ export const URI = {
     EDIT_COLLECTION: 'collection/{0}',
     DELETE_COLLECTION: 'collection/{0}',
     GET_COLLECTION_POSTS: 'collection/{0}/post',
+    GET_COLLECTION_FOLLOWERS: 'collection/{0}/followers',
 
-    GET_COLLECTION_FOLLOWERS: 'collection/{0}/favcollection',
     FOLLOW_USER: 'core/follower/',
     GET_ALL_FAV_POST: 'favPost',
     FOLLOW_COLLECTION: 'favcollection',

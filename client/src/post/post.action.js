8472e269f80a2f8e9e97a1656fa1744466436805
Surbhiairan <surbhiairan1@gmail.com>
2019-03-23 16:23:38 +0530

    getPostDetauls, getPostComments actions created

--- client/src/post/post.action.js
+++ client/src/post/post.action.js
@@ -1,5 +1,6 @@
 import { Post } from './post.constants';
 import { API_ROOT, URI } from '../config/config';
+import { StringFormat } from '../utils/StringFormat';
 
 export const createPost = (values, history) => {
     let token = JSON.parse(localStorage.getItem('user')).token;
@@ -25,6 +26,68 @@ export const createPost = (values, history) => {
         .catch(err => {
             dispatch({
                 type: Post.CREATE_POST_FAILURE,
+                payload: err
+            })
+        })
+    }else{
+        history.push('/login');
+    }
+    }
+}
+
+export const getPostComments = (id, history) => {
+    let token = JSON.parse(localStorage.getItem('user')).token;
+    return (dispatch) => {
+        if(token!=null){
+        dispatch({ type: Post.POST_COMMENTS_LOADING })
+        fetch(StringFormat(API_ROOT + URI.POST_COMMENTS, id), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            dispatch({
+                type: Post.POST_COMMENTS_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: Post.POST_COMMENTS_FAILURE,
+                payload: err
+            })
+        })
+    }else{
+        history.push('/login');
+    }
+    }
+}
+
+export const getPostDetails = (id, history) => {
+    let token = JSON.parse(localStorage.getItem('user')).token;
+    return (dispatch) => {
+        if(token!=null){
+        dispatch({ type: Post.POST_DETAIL_LOADING })
+        fetch(StringFormat(API_ROOT + URI.POST_DETAIL, id), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            dispatch({
+                type: Post.POST_DETAIL_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: Post.POST_DETAIL_FAILURE,
                 payload: err
             })
         })

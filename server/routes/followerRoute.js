4289e6a19ab8a7097c93fbe0e9e4fb4c5503bbfe
Sadanand Mandal <sadanand.mandal@centricconsulting.com>
2019-03-26 20:40:56 +0530

    followingUser API created

--- server/routes/followerRoute.js
+++ server/routes/followerRoute.js
@@ -7,6 +7,7 @@ const FollowerController = require('../controller/followerController');
 folllowerRoutes.post('/', Auth.isAuthenticated, FollowerController.followUser)
 folllowerRoutes.get('/', Auth.isAuthenticated, FollowerController.getFollowers)
 folllowerRoutes.delete('/', Auth.isAuthenticated, FollowerController.unFollowUser)
+folllowerRoutes.get('/followingUser', Auth.isAuthenticated, FollowerController.getFollowingUser)
 
 
 module.exports = folllowerRoutes;

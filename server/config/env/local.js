926a257527f56288f918c2a080da5622ef872238
surbhi airan <surbhiairan1@gmail.com>
2019-03-27 09:03:37 +0000

    local.js edited online with Bitbucket

--- server/config/env/local.js
+++ server/config/env/local.js
@@ -2,7 +2,7 @@ module.exports = {
     env: 'local',
     port: process.env.PORT || 8080,
     db: {
-      "uri": "mysql://root@43.255.154.50:3306/post_curve",
+      "uri": "mysql://postcurve:postcurve01@43.255.154.50:3306/post_curve",
       "connectionLimit": 100,
       "acquireTimeout": 100000,
       "connectTimeout": 100000

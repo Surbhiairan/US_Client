4e521969f64319f14093e0b4d54a95352f24a009
Surbhiairan <surbhiairan1@gmail.com>
2019-03-28 19:33:59 +0530

    deleted python setup

--- client/postcurve_api/core/token.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
-
-
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, core, timestamp):
-        return (six.text_type(core.pk) + six.text_type(timestamp)) +  six.text_type(core.is_active)
-account_activation_token = AccountActivationTokenGenerator()
